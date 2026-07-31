c87f1ca3dd44c287164de1daea30b880a74dd9eb
boussa-cell <bouhrama.aissa@gmail.com>
2026-07-31 21:45:38 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// ============================================================
+// هذا الملف جاهز بالفعل بقيمك الخاصة - لا تحتاج تعدل فيه شي
+// ============================================================
+const SUPABASE_URL = "https://okkhqnvxgkmlxdzmlsns.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ra2hxbnZ4Z2ttbHhkem1sc25zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU0MzEzMDQsImV4cCI6MjEwMTAwNzMwNH0.3TaZjKxPnrjQyUpnbOusUqGGWGnUxrU4AWnWudIdDMw";
